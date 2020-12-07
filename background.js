6866aaa5fc0a1005f380f7baf049eb01c04e3b10
Jonathan M <jonmays77@verizon.net>
2020-12-07 13:48:35 -0500

    test

--- background.js
+++ background.js
@@ -1,13 +1,33 @@
-var contextMenuItem = {
-    "id" : "goToLS",
-    "title" : "Open Stream in TwitchLS",
-    "contexts": ["all"]
+var contextMenus = {};
+
+contextMenus.createCounterString = 
+    chrome.contextMenus.create(
+        {"title":"Open with TwitchLS",
+        "contexts" : ["all"], "id": "trihard"
+        },
+        function (){
+            if(chrome.runtime.lastError){
+                console.error(chrome.runtime.lastError.message);
+            }
+        }
+    );
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+
+  
+function onClickHandler(info, tab) {
+  console.log(document.location.href.split('/'))
+  var link = document.location.href.split('/');
+  chrome.tabs.getSelected(null,function(tab) {
+    var tablink = tab.url;
+    console.log(tablink)
+    if(tablink.includes('twitch.tv')){
+      console.log('redirecting')
+      chrome.tabs.update({url: `https://twitchls.com/${tablink.split('/')[3]}`});
+
+    }
+    
+  }); 
+  
 };
-  
-  chrome.contextMenus.onClicked.addListener(onClickHandler);
-  
-  function onClickHandler(info, tab) {
-    var link = document.location.href.split('/');
-    window.location.replace(`https://twitchls.com/${link[3]}`)
-  };
-  
