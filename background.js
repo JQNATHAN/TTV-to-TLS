0d39612a0e81c37f5c86ba8c071259617de9aaef
Jonathan M <jonmays77@verizon.net>
2020-12-07 13:32:55 -0500

    cock

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
 var contextMenuItem = {
     "id" : "goToLS",
-    "title" : "Open in TwitchLS",
+    "title" : "Open Stream in TwitchLS",
     "contexts": ["all"]
 };
-chrome.contextMenus.create(contextMenuItem);
-
-function goToLS(){
+  
+  chrome.contextMenus.onClicked.addListener(onClickHandler);
+  
+  function onClickHandler(info, tab) {
     var link = document.location.href.split('/');
     window.location.replace(`https://twitchls.com/${link[3]}`)
-}
-
-
-chrome.contextMenus.onClicked.addListener(goToLS)
+  };
+  
